6e88d5604140f107cd29ee43220185e0838de45d
Alexei Margasov <amargasov@naumen.ru>
2014-08-11 15:51:16 +0600

    send email after save new issue

--- lib/watcher_groups_issue_hook.rb
+++ lib/watcher_groups_issue_hook.rb
@@ -8,6 +8,22 @@ module WatcherGroupsIssueHooks
 
     def controller_issues_new_after_save(context={})
       if "watcher_group_ids".in? context[:params][:issue]
+        group_ids = context[:params][:issue]["watcher_group_ids"]
+        group_ids.each do |group_id|
+          issue = Issue.find(context[:issue].id)
+          group_users = Group.find(group_id.to_i).users
+          group_users.each do |user|
+            if !user.in? issue.watcher_users
+              note = "Watcher #{user.name} was added"
+              journal = Journal.new(:journalized => issue, :user => user, :notes => note, :notify => false, :is_system_note=> true)
+              journal.save
+              if Setting.notified_events.include?('issue_updated') ||
+                        (Setting.notified_events.include?('issue_note_added') && journal.notes.present?)
+                Mailer.watcher_add(journal, user.mail).deliver
+              end
+            end
+          end
+        end
         context[:issue].watcher_groups_ids = context[:params][:issue]["watcher_group_ids"]
         context[:issue].save
       end
